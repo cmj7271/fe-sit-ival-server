a463d9bd3815dac04f6e9524150bc05c81e46ae9
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-12 07:49:20 +0900

    seat 수정

--- src/main/java/com/hongik/fe_sit_ival_server/seat/dto/GetAllSeatInfoResponse.java
+++ src/main/java/com/hongik/fe_sit_ival_server/seat/dto/GetAllSeatInfoResponse.java
@@ -1,4 +1,3 @@
 package com.hongik.fe_sit_ival_server.seat.dto;
 
-public record GetAllSeatInfoResponse(Long teamId, String name) {
-}
+public record GetAllSeatInfoResponse(Long id, String TeamName, Long horizon, Long vertical, Boolean isBooked) {}
