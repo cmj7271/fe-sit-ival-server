3ae2416efbb38bdd158d87b600d9ba02c0cf31ea
Jaewon Kim <eshcirc1e@gmail.com>
2025-01-11 16:22:49 +0900

    chore: spotless 적용 (#2)
    * .gitignore 수정
    
    * spotless 적용

--- src/main/java/com/hongik/fe_sit_ival_server/FeSitIvalServerApplication.java
+++ src/main/java/com/hongik/fe_sit_ival_server/FeSitIvalServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FeSitIvalServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FeSitIvalServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FeSitIvalServerApplication.class, args);
+    }
 }
