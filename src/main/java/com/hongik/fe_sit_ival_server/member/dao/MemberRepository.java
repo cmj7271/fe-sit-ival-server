097b4e0b17d95ffceb9dc5131e17f4348c4612eb
CheonMinJae <cmjcmj1011@gmail.com>
2025-01-12 02:40:02 +0900

    delete member

--- src/main/java/com/hongik/fe_sit_ival_server/member/dao/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hongik.fe_sit_ival_server.member.dao;
-
-import com.hongik.fe_sit_ival_server.member.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {}
